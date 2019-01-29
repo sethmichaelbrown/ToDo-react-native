a318e879e797081974c02387cc7f94a018e7f417
SMB <sethmichaelbrown@gmail.com>
2019-01-29 15:14:51 -0700

    Note and main view

---
+++ app/components/Main.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Noter</Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}>
+
+        </ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='note...'
+            placeholderTextColor='white'>
+
+          </TextInput>
+        </View>
+
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd'
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed'
+  },
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: '#E91E63',
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24
+  }
+});
