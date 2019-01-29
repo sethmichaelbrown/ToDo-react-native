b3ac20d1fd9239c32d0e457c8cf13f2e3520e6ef
SMB <sethmichaelbrown@gmail.com>
2019-01-29 15:30:28 -0700

    Adding note working, delete not working

--- app/components/Main.js
+++ app/components/Main.js
@@ -9,10 +9,24 @@ import {
   TouchableOpacity
 } from 'react-native';
 
-
+import Note from './Note'
 
 export default class App extends Component {
+
+  state = {
+    noteArray: [],
+    noteText: '',
+    noteText: ''
+  }
+
+
   render() {
+
+    const notes = this.state.noteArray.map((val, key) => {
+      return <Note key={key} keyval={key} val={val}
+        deleteNote={() => this.deleteNote(key)} />
+    })
+
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -20,25 +34,47 @@ export default class App extends Component {
         </View>
 
         <ScrollView style={styles.scrollContainer}>
-
+          {notes}
         </ScrollView>
 
         <View style={styles.footer}>
           <TextInput
             style={styles.textInput}
+            onChangeText={(noteText) => this.setState({ noteText })}
+            value={this.state.noteText}
             placeholder='note...'
             placeholderTextColor='white'>
 
           </TextInput>
         </View>
 
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity
+          onPress={this.addNote.bind(this)}
+          style={styles.addButton}>
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
 
       </View>
     );
   }
+
+ addNote(){
+        if(this.state.noteText){
+            var d = new Date();
+            this.state.noteArray.push({
+                'date':d.getFullYear()+
+                "/"+(d.getMonth()+1) +
+                "/"+ d.getDate(),
+                'note': this.state.noteText
+            });
+            this.setState({ noteArray: this.state.noteArray });
+            this.setState({noteText:''});
+        }
+    }
+    deleteNote(key){
+        this.state.noteArray.splice(key, 1);
+        this.setState({noteArray: this.state.noteArray});
+    }
 }
 
 const styles = StyleSheet.create({
@@ -59,7 +95,8 @@ const styles = StyleSheet.create({
   },
   scrollContainer: {
     flex: 1,
-    marginBottom: 100
+    marginBottom: 100,
+    backgroundColor: 'white'
   },
   footer: {
     position: 'absolute',
